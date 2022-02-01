39f6e3c1dc605f168660c3e67fdaff996d444c34
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-01-31 20:05:53 -0500

    commit tree e1770bfc17c2da60ccccd32fee634fc33ebbe82a

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function(){
+    const newarr= [...drivers];
+     return newarr.slice(0, 2)
+}
+const returnLastTwoDrivers = function(){
+    const newarr= [...drivers];
+    return newarr.slice(2, 4)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(int){
+    return function(fare){
+        return fare * int;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arr, f){
+    return f(arr);
+}
